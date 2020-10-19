36327a8b7e75f8be9d5ac107d7fa4ec99c3bfcbb
dar-ia <64155350+dar-ia@users.noreply.github.com>
2020-10-19 18:36:22 +0400

    Update statistics.py
    Check added

--- statistics.py
+++ statistics.py
@@ -1,4 +1,6 @@
 from scipy import stats
+import display
+import numpy as np
 
 def chi_sqauare(data, data_expected):
 
@@ -26,3 +28,18 @@ def u_mannwhitney(data, data_expected):
         decicion = 1
 
     return decicion
+
+
+[y1, sr1] = display.read_file("se-2.wav")
+[y2, sr2] = display.read_file("se.wav")
+
+[hist1, edges1] = np.histogram(a=y1, bins=50)
+[hist2, edges2] = np.histogram(a=y2, bins=50)
+
+[result_chi, p_chi] = chi_sqauare(data=hist1, data_expected=hist2)
+[result_t, p_t] = t_stjudent(data=y1, data_expected=y2)
+[result_u, p_u] = u_mannwhitney(data=hist1, data_expected=hist2)
+
+
+
+print("chi result: ", result_chi, " with p-value = ", p_chi, "\nstud result: ", result_t, " with p-value = ", p_t, "\nmann result: ", result_u, "with p-value = ", p_u)
