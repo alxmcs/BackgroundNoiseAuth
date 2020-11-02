98f38cd2b084742fc18d1a0adaef7d02085553a5
dar-ia <64155350+dar-ia@users.noreply.github.com>
2020-11-02 12:21:19 +0400

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -5,7 +5,7 @@ import numpy as np
 def chi_sqauare(data, data_expected):
 
     [chi, p] = stats.chisquare(f_obs=data, f_exp=data_expected)
-    decision = 0
+    decision = -1
     if p <= 0.05:
         decision = 1
 
@@ -13,8 +13,8 @@ def chi_sqauare(data, data_expected):
 
 def t_stjudent(data, data_expected):
 
-    [t, p] = stats.ttest_ind(a=data, b=data_expected)
-    decision = 0
+    [t, p] = stats.ttest_ind(a=data, b=data_expected, equal_var=False)
+    decision = -1
     if p <= 0.05:
         decision = 1
 
@@ -23,7 +23,7 @@ def t_stjudent(data, data_expected):
 def u_mannwhitney(data, data_expected):
 
     [u, p] = stats.mannwhitneyu(x=data, y=data_expected)
-    decicion = 0
+    decicion = -1
     if p<= 0.05:
         decicion = 1
 
